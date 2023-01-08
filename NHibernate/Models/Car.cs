3c13f5288b5da8d8a27da1dedd5c0732c2c30d1c
Daniel PC <daniel.barczynski@onet.pl>
2023-01-08 01:01:02 +0100

    Add project files.

---
+++ NHibernate/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernateExample.Models
+{
+    public class Car
+    {
+        public virtual int Id { get; set; }
+        public virtual string Brand { get; set; }
+        public virtual string Model { get; set; }
+    }
+}
---
+++ NHibernate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using NHibernateExample.Models;
+
+namespace NHibernateExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=(localdb)\\learning; Initial Catalog=learning; Integrated Security=true";
+            var config = new Configuration();
+            config.DataBaseIntegration(d =>
+            {
+                d.ConnectionString = connectionString;
+                d.Dialect<MsSql2012Dialect>();
+                d.Driver<SqlClientDriver>();
+            });
+
+            config.AddAssembly(Assembly.GetExecutingAssembly());
+            var sessionFactory = config.BuildSessionFactory();
+            using (var session = sessionFactory.OpenSession())
+            {
+                var car = new Car() { Brand = "Audi", Model = "R8" };
+                session.Save(car);
+            }
+        }
+    }
+}
